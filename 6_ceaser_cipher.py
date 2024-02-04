bcc4960c492f80f7654719d0df480836580987f6
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-02-04 14:14:17 +0530

    Create 6_casar_cipher.py

---
+++ 6_ceaser_cipher.py
@@ -0,0 +1,51 @@
+#CESEAR CIAPHER
+print('''     
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+           88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+''')
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z''a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# in alphabet i have taken the list 2 time because if somehow in case of incode the letter z comes it will than through error 
+string=input("enter the codeword:")
+direction=input("want to incode or decode the code word")
+number=int(input("enter the shift number"))
+
+# agar kisi pagal neh shift 26 se bada diya phir 
+if number>26:
+    number%=26
+else:
+    number == number
+    
+
+if direction == "incode":
+    def incode(plainstring,shift):
+        word=""
+        for letter in plainstring:
+            position=alphabet.index(letter)
+            new_position=position + shift
+            new_alphabet=alphabet[new_position]
+            word+=new_alphabet
+        print(word)
+    incode(plainstring=string,shift=number)
+else:
+    def decode(plainstring,shift):
+        word=""
+        for letter in plainstring:
+            position=alphabet.index(letter)
+            new_position=position - shift
+            new_alphabet=alphabet[new_position]
+            word+=new_alphabet
+        print(word)
+    decode(plainstring=string,shift=number)
