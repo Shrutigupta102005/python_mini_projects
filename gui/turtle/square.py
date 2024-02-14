c7eec178c91369a9439d2a14d4c8e22adebec114
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-02-14 19:05:22 +0530

    Create square.py

---
+++ gui/turtle/square.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.home()
+
+
+screen = Screen()
+screen.exitonclick()
